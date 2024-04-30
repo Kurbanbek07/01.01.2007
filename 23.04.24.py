7dca4fe8e0f25d997df4c9f24507fae0ed874d2f
Kurbanbek07 <kkurbanbek67@gmail.com>
2024-04-30 15:40:02 +0600

    first commit

---
+++ 23.04.24.py
@@ -0,0 +1,18 @@
+def find_num(list1):
+    ind = 0
+    for i in list1:
+        if i == 6:
+            return ind
+        ind += 1
+
+print(find_num([8, 34, 6, 7, 67, 3, 5, 8]))
+a=6
+print ("элемент саны", a)
+
+def find_num(list1):
+    for i in range(len(list1)):
+        if list1[i] == 6:
+            return i
+print(find_num([8,43,94,6,3]))
+a=6
+print("element sani",a)
